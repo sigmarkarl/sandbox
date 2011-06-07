2f631b2ec8cd081472b2518fcdb6b2ab84ae30d8
Sigmar Stefansson <sigmarkarl@gmail.com>
2011-06-07 12:44:51 +0200

    stuff

--- gwtgl/src/org/simmi/mapviewer/client/Mapviewer.java
+++ gwtgl/src/org/simmi/mapviewer/client/Mapviewer.java
@@ -1,29 +1,19 @@
 package org.simmi.mapviewer.client;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.maps.client.InfoWindowContent;
-import com.google.gwt.maps.client.MapWidget;
-import com.google.gwt.maps.client.Maps;
-import com.google.gwt.maps.client.control.LargeMapControl;
-import com.google.gwt.maps.client.event.MarkerClickHandler;
-import com.google.gwt.maps.client.geom.LatLng;
-import com.google.gwt.maps.client.overlay.Icon;
-import com.google.gwt.maps.client.overlay.Marker;
-import com.google.gwt.maps.client.overlay.MarkerOptions;
 import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -32,6 +22,7 @@ import com.google.gwt.user.client.ui.IntegerBox;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -44,26 +35,69 @@ public class Mapviewer implements EntryPoint {
 	String locs = null;
 	
 	public void onModuleLoad() {
-		Maps.loadMapsApi("", "2", false, new Runnable() {
-			public void run() {
-				buildUi();
-		    }
-		});
+		doStuff();
 	}
 	
 	public native void console( String str ) /*-{
 		$wnd.console.log( str );
 	}-*/;
 	
-	List<Marker>	markersArray = new ArrayList<Marker>();
+	public native JavaScriptObject newArray() /*-{
+		return [];
+	}-*/;
+	
+	public native JavaScriptObject initialize( JavaScriptObject mapcanvas ) /*-{
+	    var latlng = new $wnd.google.maps.LatLng(-34.397, 150.644);
+	    var myOptions = {
+	      zoom: 2,
+	      center: latlng,
+	      mapTypeId: $wnd.google.maps.MapTypeId.ROADMAP
+	    };
+	    var map = new $wnd.google.maps.Map( mapcanvas, myOptions );
+	    
+	    return map;
+	  }-*/;
+	
+	public native void npos( JavaScriptObject map, JavaScriptObject markersArray, String str, double lat, double lng, String contentString, String imurl ) /*-{
+		  var pos = new $wnd.google.maps.LatLng( lat, lng );
+		  var markerOpts = {
+				title: str,
+		    	position: pos,
+		    	map: map,
+		    	icon: imurl
+		  };
+		  var marker = new $wnd.google.maps.Marker( markerOpts );
+		  markersArray.push( marker );
+		  
+		  var infowindow = new $wnd.google.maps.InfoWindow({
+			    content: contentString
+		  });
+		  
+		  $wnd.google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.open(map,marker);
+		  });
+	  }-*/;
+	  
+	  public native JavaScriptObject clearMarkers( JavaScriptObject markersArray ) /*-{
+		  if( markersArray != null ) {
+			  for (i in markersArray) {
+			      markersArray[i].setMap(null);
+			  }
+		  }
+		  markersArray = [];
+		  
+		  return markersArray;
+	  }-*/;
+	
+	/*List<Marker>	markersArray = new ArrayList<Marker>();
 	public void clearMarkers( MapWidget map ) {
 		for( Marker marker : markersArray ) {
 			map.removeOverlay( marker );
 		}
 		markersArray.clear();
-	}
+	}*/
 	
-	public void pos( final MapWidget map, String str, double lat, double lng, final String contentString, String imurl ) {
+	/*public void pos( final MapWidget map, String str, double lat, double lng, final String contentString, String imurl ) {
 		  LatLng pos = LatLng.newInstance( lat, lng );
 		  Icon icon = Icon.newInstance(imurl);
 		  MarkerOptions markerOptions = MarkerOptions.newInstance( icon );
@@ -86,16 +120,16 @@ public class Mapviewer implements EntryPoint {
 		  
 		  google.maps.event.addListener(marker, 'click', function() {
 			  infowindow.open(map,marker);
-		  });*/
-	  }
+		  });*
+	  }*/
 	
-	public void load( MapWidget map, Map<String,LatLng> m, int mintemp, int maxtemp, int phmin, int phmax, String selitem ) {
+	public void load( JavaScriptObject map, Map<String,LatLong> m, int mintemp, int maxtemp, int phmin, int phmax, String selitem ) {
 		//InputStream is = this.getClass().getResourceAsStream("/locs.txt");
 		//BufferedReader br = new BufferedReader( new InputStreamReader(is) );
 		//Random r = new Random();
 		//JSObject jo = JSObject.getWindow(this);
 		//jo.call("clearMarkers", new Object[] {});
-		clearMarkers( map );
+		nmarkersArray = clearMarkers( nmarkersArray );
 		
 		Canvas canvas = Canvas.createIfSupported();
 		canvas.setCoordinateSpaceWidth(16);
@@ -110,6 +144,8 @@ public class Mapviewer implements EntryPoint {
 		g.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC );
 		g.setRenderingHint( RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE );*/
 		//g.setRenderingHint( RenderingHints.KEY_ );
+		
+		//map.checkResize();
 		
 		String[] lines = locs.split("\n");
 		for( String line : lines ) {
@@ -163,8 +199,8 @@ public class Mapviewer implements EntryPoint {
 				context2d.setFillStyle( color );
 				//g.setColor( new Color( 200+(int)(15*(ph-7.0)), 200-(int)(15*(ph-7.0)), 0 ) );
 				context2d.beginPath();
-				context2d.moveTo(0.0, 0.0);
-				context2d.arc( 8, 16, 16, Math.PI/2.0, 2.0*Math.PI/3.0 );
+				context2d.moveTo(8.0, 16.0);
+				context2d.arc( 8, 16, 15, 3.0*Math.PI/2.0, 5.0*Math.PI/3.0 );
 				context2d.closePath();
 				context2d.fill();
 				//fillArc(-8, 0, 32, 32, 60, 30);
@@ -176,30 +212,30 @@ public class Mapviewer implements EntryPoint {
 				context2d.setFillStyle( color );
 				
 				context2d.beginPath();
-				context2d.moveTo(0.0, 0.0);
+				context2d.moveTo(8.0, 16.0);
 				//context2d.arc( 8, 8, 8, 0, 2*Math.PI );//Math.PI/2.0, 2.0*Math.PI/3.0);
-				context2d.arc(8, 16, 16, Math.PI/3.0, Math.PI/2.0);
+				context2d.arc(8, 16, 15, 4.0*Math.PI/3.0, 3.0*Math.PI/2.0);
 				context2d.closePath();
 				context2d.fill();
 				
 				//g.setColor( Color.black );
 				//g.drawArc(-8, 0, 32, 32, 60, 60);
 				color = "#000000";
-				context2d.setFillStyle( color );
+				context2d.setStrokeStyle( color );
 				context2d.beginPath();
-				context2d.moveTo(0.0, 0.0);
-				context2d.arc(8, 16, 16, Math.PI/3.0, 2.0*Math.PI/3.0 );
+				context2d.moveTo(8.0, 16.0);
+				context2d.arc(8, 16, 15, 4.0*Math.PI/3.0, 5.0*Math.PI/3.0 );
 				context2d.closePath();
 				context2d.stroke();
 				
 				String dataurl = canvas.toDataUrl();
 				console( dataurl );
 				
-				LatLng ll = null;
+				LatLong ll = null;
 				if( m.containsKey(split[0]) ) {
 					ll = m.get(split[0]);
 					//jo.call("pos", new Object[] {split[split.length-1]+" Ph:"+split[3]+" Temp:"+split[2],ll.lat+r.nextDouble()/100.0,ll.lng+r.nextDouble()/100.0+"",content, "data:image/png;base64,"+dataurl});
-					pos( map, split[split.length-1]+" Ph:"+split[3]+" Temp:"+split[2],ll.getLatitude()+Random.nextDouble()/100.0,ll.getLongitude()+Random.nextDouble()/100.0,content, dataurl );
+					npos( map, nmarkersArray, split[split.length-1]+" Ph:"+split[3]+" Temp:"+split[2],ll.getLatitude()+Random.nextDouble()/100.0,ll.getLongitude()+Random.nextDouble()/100.0,content, dataurl );
 				} else {
 					Window.alert( split[0] );
 				}
@@ -207,9 +243,32 @@ public class Mapviewer implements EntryPoint {
 		}
 	}
 	
-	public void doStuff( final MapWidget map ) {
-		final Map<String,LatLng>	m = new HashMap<String,LatLng>();
-		m.put("Hveragerdi", LatLng.newInstance(63.99,-21.10));
+	class LatLong {
+		public LatLong( double lat, double lng ) {
+			this.lat = lat;
+			this.lng = lng;
+		}
+		
+		double	lat;
+		double	lng;
+		
+		public double getLatitude() {
+			return lat;
+		}
+		
+		public double getLongitude() {
+			return lng;
+		}
+	};
+	
+	JavaScriptObject nmarkersArray = null;
+	public void doStuff() {
+		final SimplePanel	mapcanvas = new SimplePanel();
+		final JavaScriptObject map = initialize( mapcanvas.getElement() );
+		
+		final Map<String,LatLong>	m = new HashMap<String,LatLong>();
+		//final Map<String,LatLng>	m = new HashMap<String,LatLng>();
+		/*m.put("Hveragerdi", LatLng.newInstance(63.99,-21.10));
 		m.put("Hveravellir", LatLng.newInstance(64.45,-19.33));
 		m.put("Oxarfjordur", LatLng.newInstance(66.11,-16.75));
 		m.put("Hrafntinnusker", LatLng.newInstance(63.55,-19.09));
@@ -219,7 +278,18 @@ public class Mapviewer implements EntryPoint {
 		m.put("Reykjanes", LatLng.newInstance(63.88,-22.42));
 		m.put("USA", LatLng.newInstance(43.88,-22.42));
 		m.put("Japan", LatLng.newInstance(43.88,-2.42));
-		m.put("South-africa", LatLng.newInstance(23.88,-32.42));
+		m.put("South-africa", LatLng.newInstance(23.88,-32.42));*/
+		m.put("Hveragerdi", new LatLong(63.99,-21.10));
+		m.put("Hveravellir", new LatLong(64.45,-19.33));
+		m.put("Oxarfjordur", new LatLong(66.11,-16.75));
+		m.put("Hrafntinnusker", new LatLong(63.55,-19.09));
+		m.put("Geysir area", new LatLong(64.19,-20.18));
+		m.put("Snaefellsnes", new LatLong(64.65,-22.15));
+		m.put("Hagongur", new LatLong(64.32,-18.12));
+		m.put("Reykjanes", new LatLong(63.88,-22.42));
+		m.put("USA", new LatLong(44.97, -110.71));
+		m.put("Japan", new LatLong(35.23,139.10));
+		m.put("South-africa", new LatLong(-25.56,22.06));
 		
 		final ListBox	combobox = new ListBox();
 		HorizontalPanel toolbar = new HorizontalPanel();
@@ -244,7 +314,7 @@ public class Mapviewer implements EntryPoint {
 		phmaxspin.setValue( 12 );
 		toolbar.add( phmaxspin );
 		
-		ValueChangeHandler<Integer> cl = new ValueChangeHandler<Integer>() {
+		ValueChangeHandler<Integer> cl = new ValueChangeHandler<Integer>() {			
 			@Override
 			public void onValueChange(ValueChangeEvent<Integer> event) {
 				load( map, m, (Integer)minspin.getValue(), (Integer)maxspin.getValue(), (Integer)phminspin.getValue(), (Integer)phmaxspin.getValue(), (String)combobox.getValue( combobox.getSelectedIndex() ) );
@@ -280,13 +350,14 @@ public class Mapviewer implements EntryPoint {
 		//int w = Window.getClientWidth();
 		int h = Window.getClientHeight();
 		
-		map.setHeight( (h-25)+"px" );
+		mapcanvas.setHeight( (h-25)+"px" );
 		
 		Window.addResizeHandler( new ResizeHandler() {
 			@Override
 			public void onResize(ResizeEvent event) {
 				int h = event.getHeight();
-				map.setHeight( (h-25)+"px" );
+				mapcanvas.setHeight( (h-25)+"px" );
+				//map.checkResizeAndCenter();
 			}
 		});
 		
@@ -294,7 +365,7 @@ public class Mapviewer implements EntryPoint {
 		//vp.setSize(w+"px", h+"px");
 		vp.setWidth("100%");
 		vp.add( toolbar );
-		vp.add( map );
+		vp.add( mapcanvas );
 		RootPanel.get().add( vp );
 		
 		try {
@@ -314,11 +385,14 @@ public class Mapviewer implements EntryPoint {
 	}
 
 	private void buildUi() {
-	    LatLng cawkerCity = LatLng.newInstance(39.509, -98.434);
-	    final MapWidget map = new MapWidget(cawkerCity, 2);
-	    map.addControl(new LargeMapControl());
+	    //LatLng cawkerCity = LatLng.newInstance(39.509, -98.434);
+	    //final MapWidget map = new MapWidget(cawkerCity, 2);
+	    //map.addControl(new LargeMapControl());
+		
+		SimplePanel	mapcanvas = new SimplePanel();
+		JavaScriptObject map = initialize( mapcanvas.getElement() );
 	    
-		doStuff( map );
+		//doStuff( map );
 
 	    /*map.addOverlay(new Marker(cawkerCity));
 	    map.getInfoWindow().open(map.getCenter(), new InfoWindowContent("World's Largest Ball of Sisal Twine"));
