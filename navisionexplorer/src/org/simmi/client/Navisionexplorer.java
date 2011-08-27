600a79e2f240deccfb3e24629d66d79cdb44cbad
root <root@simmi.(none)>
2011-08-27 18:12:02 +0000

    all

--- navisionexplorer/src/org/simmi/client/Navisionexplorer.java
+++ navisionexplorer/src/org/simmi/client/Navisionexplorer.java
@@ -1,23 +1,12 @@
 package org.simmi.client;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.SplitLayoutPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
 import com.google.gwt.visualization.client.DataView;
@@ -56,76 +45,77 @@ public class Navisionexplorer implements EntryPoint {
 		final SplitLayoutPanel slp = new SplitLayoutPanel();
 		
 		Runnable onLoadCallback = new Runnable() {
-		      public void run() {
-		    	  data = DataTable.create();
-		    	  data.addColumn( ColumnType.STRING, "Starfsmaður");
-		    	  data.addColumn( ColumnType.STRING, "Kennitala");
+			public void run() {
+				data = DataTable.create();
+		    	data.addColumn( ColumnType.STRING, "Starfsmaður");
+		    	data.addColumn( ColumnType.STRING, "Kennitala");
 		    	  
-		    	  options = Options.create();
-		    	  //options.setWidth("100px");
-		    	  //options.setHeight("480px");
-		    	  options.setAllowHtml( true );
+		    	options = Options.create();
+		    	//options.setWidth("100px");
+		    	//options.setHeight("480px");
+		    	options.setAllowHtml( true );
 		    	  
-		    	  view = DataView.create( data );
-		    	  table = new Table( view, options );
+		    	view = DataView.create( data );
+		    	table = new Table( view, options );
 		    	  
-		    	  greetingService.getAllUsers( new AsyncCallback<List<Person>>() {
-					@Override
-					public void onSuccess(List<Person> result) {						
-						for( Person person : result ) {
-							int r = data.getNumberOfRows();
-							data.addRow();
-							
-							data.setValue( r, 0, person.getName() );
-							data.setValue( r, 1, person.getKt() );
-
-							view = DataView.create( data );
-							table.draw( view, options );
+		    	greetingService.getAllUsers( new AsyncCallback<List<Person>>() {
+			    	@Override
+					public void onSuccess(List<Person> result) {			
+						if( result != null ) {
+				    		for( Person person : result ) {
+								int r = data.getNumberOfRows();
+								data.addRow();
+								
+								data.setValue( r, 0, person.getName() );
+								data.setValue( r, 1, person.getKt() );
+								view = DataView.create( data );
+								table.draw( view, options );
+							}
 						}
 					}
-			
+				
 					@Override
 					public void onFailure(Throwable caught) {
-						
+							
 					}
-		    	  });
-		    	  //dropHandler( table.getElement() );
+			    });
+		    //dropHandler( table.getElement() );
 		    	  
-		    	  /*vp.setVerticalAlignment( VerticalPanel.ALIGN_MIDDLE );
-		    	  vp.setHorizontalAlignment( VerticalPanel.ALIGN_CENTER );    	  
+		    /*vp.setVerticalAlignment( VerticalPanel.ALIGN_MIDDLE );
+		    vp.setHorizontalAlignment( VerticalPanel.ALIGN_CENTER );    	  
 		    	  
-		    	  VerticalPanel	subvp = new VerticalPanel();
-		    	  subvp.setVerticalAlignment( VerticalPanel.ALIGN_MIDDLE );
-		    	  subvp.setHorizontalAlignment( VerticalPanel.ALIGN_CENTER );
+		    VerticalPanel	subvp = new VerticalPanel();
+		    subvp.setVerticalAlignment( VerticalPanel.ALIGN_MIDDLE );
+		    subvp.setHorizontalAlignment( VerticalPanel.ALIGN_CENTER );
 		    	  
-		    	  final HTML html = new HTML();
-		    	  //html.setText( "Dragðu skrána með smásögunni þinni í töfluna. <br>Ef þú ert logguð/loggaður inná facebook er réttur höfundur skráður. <br>Þú getur valið höfundarnafn, nafnið á raunverulegum höfundi þarf ekki að vera valið" );
-		    	  html.setHTML( "Drag-drop the file containing you short story into the table. <br>" +
-		    	  		"If you are logged into facebook, you are registered as the author. <br>" +
-		    	  		"You can choose you own authorname, it doesn't have to be your real name" );
-		    	  html.setWidth("100%");
-		    	  html.getElement().getStyle().setMargin(20.0, Unit.PX);
+		    final HTML html = new HTML();
+		    //html.setText( "Dragðu skrána með smásögunni þinni í töfluna. <br>Ef þú ert logguð/loggaður inná facebook er réttur höfundur skráður. <br>Þú getur valið höfundarnafn, nafnið á raunverulegum höfundi þarf ekki að vera valið" );
+		    html.setHTML( "Drag-drop the file containing you short story into the table. <br>" +
+		    "If you are logged into facebook, you are registered as the author. <br>" +
+		    "You can choose you own authorname, it doesn't have to be your real name" );
+		    html.setWidth("100%");
+		    html.getElement().getStyle().setMargin(20.0, Unit.PX);
 		    	  
-		    	  HTML title = new HTML("<h2>Shortstories<h2/>");
-		    	  subvp.add( title );
-		    	  HTML subtitle = new HTML("<h4>Brought to you by The Basement At 5 o'Clock reading club<h4/>");
-		    	  subvp.add( subtitle );
+		    HTML title = new HTML("<h2>Shortstories<h2/>");
+		    subvp.add( title );
+		    HTML subtitle = new HTML("<h4>Brought to you by The Basement At 5 o'Clock reading club<h4/>");
+		    subvp.add( subtitle );
 		    	  
-		    	  SimplePanel log = new SimplePanel();
-		    	  SimplePanel gug = new SimplePanel();
-		    	  com.google.gwt.dom.client.Element plus = Document.get().createElement("g:plusone");
-		  		  plus.setAttribute("size", "small");
-		    	  gug.getElement().appendChild( plus );
+		    SimplePanel log = new SimplePanel();
+		    SimplePanel gug = new SimplePanel();
+		    com.google.gwt.dom.client.Element plus = Document.get().createElement("g:plusone");
+		  	plus.setAttribute("size", "small");
+		    gug.getElement().appendChild( plus );
 		  		  
-		    	  HorizontalPanel	sharehp = new HorizontalPanel();
-		    	  sharehp.add( log );
-		    	  sharehp.add( gug );
-		    	  subvp.add( sharehp );
+		    HorizontalPanel	sharehp = new HorizontalPanel();
+		    sharehp.add( log );
+		    sharehp.add( gug );
+		    subvp.add( sharehp );
 		    	  
-		    	  subvp.add( html );
-		    	  subvp.add( focuspanel );
+		    subvp.add( html );
+		    subvp.add( focuspanel );
 		    	  
-		    	  Anchor	a = new Anchor( "huldaeggerts@gmail.com" );
+		    Anchor	a = new Anchor( "huldaeggerts@gmail.com" );
 		    	  a.setHref("mailto:huldaeggers@gmail.com");
 		    	  Anchor	fast = new Anchor( "http://fasteignaverd.appspot.com" );
 		    	  fast.setHref("http://fasteignaverd.appspot.com");
