bb7727a0c0ed2bce3cf12c1bb292277d291c76c0
root <root@simmi.(none)>
2011-08-30 19:39:30 +0000

    stuff

---
+++ blastic/src/org/simmi/client/GreetingService.java
@@ -0,0 +1,16 @@
+package org.simmi.client;
+
+import org.simmi.shared.Sequences;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String name) throws IllegalArgumentException;
+	Sequences[] getSequences();
+	String saveSequences(Sequences seqs);
+}
