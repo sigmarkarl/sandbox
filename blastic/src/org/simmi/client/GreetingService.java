7154d336e0ab614bc62f496cc532c53337f500cd
root <root@simmi.(none)>
2011-09-02 17:07:59 +0000

    commi

--- blastic/src/org/simmi/client/GreetingService.java
+++ blastic/src/org/simmi/client/GreetingService.java
@@ -1,5 +1,7 @@
 package org.simmi.client;
 
+import org.simmi.shared.Blast;
+import org.simmi.shared.Database;
 import org.simmi.shared.Sequences;
 
 import com.google.gwt.user.client.rpc.RemoteService;
@@ -13,4 +15,8 @@ public interface GreetingService extends RemoteService {
 	String greetServer(String name) throws IllegalArgumentException;
 	Sequences[] getSequences();
 	String saveSequences(Sequences seqs);
+	Blast[] getBlastResults();
+	Database[] getDatabases();
+	String saveDb(Database b);
+	String saveBlast(Blast b);
 }
