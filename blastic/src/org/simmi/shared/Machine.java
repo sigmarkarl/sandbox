94778ba76ac185d39fe6364a83d9545e326a3995
root <root@simmi.(none)>
2011-09-19 22:53:18 +0000

    ok

---
+++ blastic/src/org/simmi/shared/Machine.java
@@ -0,0 +1,48 @@
+package org.simmi.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class Machine implements IsSerializable {
+	public String getName() {
+		return name;
+	}
+	
+	public int getProcs() {
+		return nproc;
+	}
+	
+	public void setProcs( int procs ) {
+		this.nproc = procs;
+	}
+
+	String name;
+	String key;
+	int nproc;
+	boolean	on;
+	
+	public Machine() {
+		
+	}
+	
+	public Machine( String name, int proc, boolean on ) {
+		this.name = name;
+		this.nproc = proc;
+		this.on = on;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public void setKey( String key ) {
+		this.key = key;
+	}
+	
+	public boolean getOn() {
+		return on;
+	}
+	
+	public void setOn( boolean on ) {
+		this.on = on;
+	}
+};
