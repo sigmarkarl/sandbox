e65af8e944618c419c6f5303635fa1376fb0c489
root <root@simmi.(none)>
2011-09-28 23:59:06 +0000

    mm

--- Taxonomy/src/org/simmi/client/Taxonomy.java
+++ Taxonomy/src/org/simmi/client/Taxonomy.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.DragEndEvent;
 import com.google.gwt.event.dom.client.DragEndHandler;
 import com.google.gwt.event.dom.client.DragEnterEvent;
@@ -27,6 +29,7 @@ import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.DialogBox.Caption;
@@ -56,7 +59,7 @@ public class Taxonomy implements EntryPoint {
 	Map<String,TreeItem>	treemap = new HashMap<String,TreeItem>();
 	
 	//private final String server = "http://www.matis.is/taxonomy/";
-	private final String server = "http://130.208.252.34/";
+	//private final String server = "http://130.208.252.34/";
 	
 	public void recursiveNames( TreeItem item, StringBuilder sb ) {
 		for( int i = 0; i < item.getChildCount(); i++ ) {
@@ -71,11 +74,58 @@ public class Taxonomy implements EntryPoint {
 		}
 	}
 	
+	public void runSubStuff( RequestBuilder rb, final TreeItem rootitem ) throws RequestException {
+		rb.sendRequest("", new RequestCallback() {
+			
+			@Override
+			public void onResponseReceived(Request request, Response response) {
+				String resp = response.getText();
+				stuff( resp, rootitem );
+			}
+			
+			@Override
+			public void onError(Request request, Throwable exception) {
+				
+			}
+		});
+	}
+	
+	public void runStuff( String server, Tree tree ) {
+		final TreeItem	rootitem3 = tree.addItem( "root3" );
+		final TreeItem	rootitem4 = tree.addItem( "root4" );
+		final TreeItem	rootitem5 = tree.addItem( "root5" );
+		final TreeItem	rootitem6 = tree.addItem( "root6" );
+		final TreeItem	rootitem13 = tree.addItem( "root13" );
+		final TreeItem	rootitem14 = tree.addItem( "root14" );
+		
+		RequestBuilder rb3 = new RequestBuilder( RequestBuilder.GET, "http://"+server+"/3v1.txt" );
+		RequestBuilder rb4 = new RequestBuilder( RequestBuilder.GET, "http://"+server+"/4v1.txt" );
+		RequestBuilder rb5 = new RequestBuilder( RequestBuilder.GET, "http://"+server+"/5v4.txt" );
+		RequestBuilder rb6 = new RequestBuilder( RequestBuilder.GET, "http://"+server+"/6v1.txt" );
+		RequestBuilder rb13 = new RequestBuilder( RequestBuilder.GET, "http://"+server+"/13v1.txt" );
+		RequestBuilder rb14 = new RequestBuilder( RequestBuilder.GET, "http://"+server+"/14v1.txt" );
+		try {
+			runSubStuff(rb3, rootitem3);
+			runSubStuff(rb4, rootitem4);
+			runSubStuff(rb5, rootitem5);
+			runSubStuff(rb6, rootitem6);
+			runSubStuff(rb13, rootitem13);
+			runSubStuff(rb14, rootitem14);
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		final String server = Location.getHost();
+		
 		RootPanel		rp = RootPanel.get();
+		Style rootstyle = rp.getElement().getStyle();
+		rootstyle.setMargin(0.0, Unit.PX);
+		rootstyle.setPadding(0.0, Unit.PX);
 		
 		int w = Window.getClientWidth();
 		int h = Window.getClientHeight();
@@ -95,6 +145,15 @@ public class Taxonomy implements EntryPoint {
 				while( parent != null ) {
 					selectedtree = parent;
 					parent = selectedtree.getParentItem();
+				}
+				String rootname = selectedtree.getText();
+				if( rootname.contains("root") ) {
+					String[] rsplit = rootname.split(" ");
+					try {
+						searchnum = Integer.parseInt( rsplit[0].substring(4) );
+					} catch( Exception e ) {
+						
+					}
 				}
 				
 				String qstr =  sb.toString();
@@ -129,102 +188,17 @@ public class Taxonomy implements EntryPoint {
 		//tree.getElement().getStyle().setBorderColor("#aa4444");
 		//tree.getElement().getStyle().setBorderWidth( 2.0, Unit.PX );
 		
-		final TreeItem	rootitem3 = tree.addItem( "root3" );
-		final TreeItem	rootitem4 = tree.addItem( "root4" );
-		final TreeItem	rootitem5 = tree.addItem( "root5" );
-		final TreeItem	rootitem6 = tree.addItem( "root6" );
-		final TreeItem	rootitem13 = tree.addItem( "root13" );
-		final TreeItem	rootitem14 = tree.addItem( "root14" );
-		
-		RequestBuilder rb3 = new RequestBuilder( RequestBuilder.GET, server+"3v1.txt" );
-		RequestBuilder rb4 = new RequestBuilder( RequestBuilder.GET, server+"4v1.txt" );
-		RequestBuilder rb5 = new RequestBuilder( RequestBuilder.GET, server+"5v4.txt" );
-		RequestBuilder rb6 = new RequestBuilder( RequestBuilder.GET, server+"6v1.txt" );
-		RequestBuilder rb13 = new RequestBuilder( RequestBuilder.GET, server+"13v1.txt" );
-		RequestBuilder rb14 = new RequestBuilder( RequestBuilder.GET, server+"14v1.txt" );
-		try {
-			rb3.sendRequest("", new RequestCallback() {
+		greetingService.getRemoteAddress( new AsyncCallback<String>() {
+			@Override
+			public void onFailure(Throwable caught) {
 				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					String resp = response.getText();
-					stuff( resp, rootitem3 );
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					
-				}
-			});
-			rb4.sendRequest("", new RequestCallback() {
-				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					String resp = response.getText();
-					stuff( resp, rootitem4 );
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					
-				}
-			});
-			rb5.sendRequest("", new RequestCallback() {
-				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					String resp = response.getText();
-					stuff( resp, rootitem5 );
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					
-				}
-			});
-			rb6.sendRequest("", new RequestCallback() {
-				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					String resp = response.getText();
-					stuff( resp, rootitem6 );
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					
-				}
-			});
-			rb13.sendRequest("", new RequestCallback() {
-				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					String resp = response.getText();
-					stuff( resp, rootitem13 );
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					
-				}
-			});
-			rb14.sendRequest("", new RequestCallback() {
-				
-				@Override
-				public void onResponseReceived(Request request, Response response) {
-					String resp = response.getText();
-					stuff( resp, rootitem14 );
-				}
-				
-				@Override
-				public void onError(Request request, Throwable exception) {
-					
-				}
-			});
-		} catch (RequestException e) {
-			e.printStackTrace();
-		}
-		
+			}
+
+			@Override
+			public void onSuccess(String result) {
+				if( result != null && result.contains("130.208.252.") ) runStuff( server, tree );
+			}			
+		});
 		FocusPanel	focus = new FocusPanel( tree );
 		//focus.setSize("100%", "100%");
 		
@@ -273,7 +247,7 @@ public class Taxonomy implements EntryPoint {
 			public void onDrop(DropEvent event) {
 				String str = event.getData("text/plain");
 				
-				stuff( str, rootitem5 );
+				//stuff( str, rootitem5 );
 			}
 		});
 		
