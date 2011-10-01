54be66fbe596230d1e417dee4da2f275f25eca41
root <root@simmi.(none)>
2011-10-01 14:01:19 +0000

    k

--- pontun/src/org/simmi/server/GreetingServiceImpl.java
+++ pontun/src/org/simmi/server/GreetingServiceImpl.java
@@ -1,48 +1,45 @@
 package org.simmi.server;
 
+import java.util.List;
+
 import org.simmi.client.GreetingService;
-import org.simmi.shared.FieldVerifier;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class GreetingServiceImpl extends RemoteServiceServlet implements
-		GreetingService {
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
 
 	public String greetServer(String input) throws IllegalArgumentException {
-		// Verify that the input is valid. 
-		if (!FieldVerifier.isValidName(input)) {
-			// If the input is not valid, throw an IllegalArgumentException back to
-			// the client.
-			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
-		}
-
-		String serverInfo = getServletContext().getServerInfo();
-		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-
-		// Escape data from the client to avoid cross-site script vulnerabilities.
-		input = escapeHtml(input);
-		userAgent = escapeHtml(userAgent);
-
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Entity ent = new Entity("verk");
+		ent.setProperty("mix", input);
+		
+		datastore.put( ent );
+		
+		return input;
 	}
 
-	/**
-	 * Escape an html string. Escaping data received from the client helps to
-	 * prevent cross-site script vulnerabilities.
-	 * 
-	 * @param html the html string to escape
-	 * @return the escaped string
-	 */
-	private String escapeHtml(String html) {
-		if (html == null) {
-			return null;
+	@Override
+	public String getAllVerk() throws IllegalArgumentException {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query("verk");
+		List<Entity> dEntities = datastore.prepare( query ).asList(FetchOptions.Builder.withDefaults());
+		
+		StringBuilder ret = new StringBuilder();
+		
+		for( Entity e : dEntities ) {
+			if( ret.length() == 0 ) ret.append( (String)e.getProperty("mix") );
+			else ret.append( "\n"+(String)e.getProperty("mix") );
 		}
-		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
-				.replaceAll(">", "&gt;");
+		
+		return ret.toString();
 	}
 }
