dc7581ca227506e41202a61b6898c3763b45e54e
root <root@simmi.(none)>
2011-10-03 20:16:58 +0000

    hm

--- hvaderimatnum/src/org/simmi/client/Hvaderimatnum.java
+++ hvaderimatnum/src/org/simmi/client/Hvaderimatnum.java
@@ -2,7 +2,6 @@ package org.simmi.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
@@ -11,7 +10,15 @@ import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.Window.Location;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
+import com.google.gwt.visualization.client.DataTable;
+import com.google.gwt.visualization.client.DataView;
+import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.events.SelectHandler;
+import com.google.gwt.visualization.client.visualizations.Table;
+import com.google.gwt.visualization.client.visualizations.Table.Options;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -52,8 +59,47 @@ public class Hvaderimatnum implements EntryPoint {
 	}-*/;
 	
 	public void showTable( String str ) {
+		console("erm");
 		
+		DialogBox	db = new DialogBox();
+		
+		db.setAutoHideEnabled( true );
+		db.getCaption().setText("Copy data");
+		db.setSize(800+"px", 600+"px");
+		
+		data = DataTable.create();
+		String[] split = str.split("\n");
+		String[] subs = split[0].split("\t");
+		
+		for( String col : subs ) {
+			data.addColumn(ColumnType.STRING, col);
+		}
+		
+		for( int i = 1; i < subs.length; i++ ) {
+			subs = split[i].split("\t");
+			
+			int r = data.getNumberOfRows();
+			data.addRow();
+			
+			int k = 0;
+			for( String s : subs ) {
+				data.setValue( r, k++, s );
+			}
+		}
+		view = DataView.create( data );
+		table.draw( view, options );
+		
+		db.add( table );
+		
+		console("muu");
+		db.center();
+		console("done");
 	}
+	
+	DataTable	data;
+	DataView	view;
+	Table		table;
+	Options		options;
 
 	/**
 	 * This is the entry point method.
@@ -83,6 +129,30 @@ public class Hvaderimatnum implements EntryPoint {
 			}
 		});
 		
+		Runnable onLoadCallback = new Runnable() {
+			public void run() {
+				data = DataTable.create();
+		    	//data.addColumn( ColumnType.STRING, "Starfsmaður");
+		    	//data.addColumn( ColumnType.STRING, "Kennitala");
+		    	  
+		    	options = Options.create();
+		    	options.setWidth("100%");
+		    	options.setHeight("100%");
+		    	options.setAllowHtml( true );
+		    	  
+		    	view = DataView.create( data );
+		    	table = new Table( view, options );
+		    	
+		    	table.addSelectHandler( new SelectHandler() {
+					@Override
+					public void onSelect(SelectEvent event) {
+						//selectEvent();
+					}
+		    	});
+			}	
+	    };
+	    VisualizationUtils.loadVisualizationApi(onLoadCallback, Table.PACKAGE);
+		
 		//ScriptElement se = Document.get().createScriptElement();
 		/*Style scriptstyle = se.getStyle();
 		scriptstyle.setMargin(0.0, Unit.PX);
@@ -97,14 +167,41 @@ public class Hvaderimatnum implements EntryPoint {
 		ae.setAttribute("width", "100%");
 		ae.setAttribute("height", "100%");
 		ae.setAttribute("codebase", "http://"+server+"/");
-		ae.setAttribute("archive", "order.jar,sqljdbc4.jar,sqljdbc_auth.jar,mail.jar");
+		ae.setAttribute("archive", "isgem.jar,swingx-core-1.6.2.jar,javaws.jar");
 		ae.setAttribute("code", "org.simmi.SortTable");
-		ae.setAttribute("id", "order");
-		ae.setAttribute("name", "order");
+		ae.setAttribute("id", "isgem");
+		ae.setAttribute("name", "isgem");
+		
 		Element pe = Document.get().createElement("param");
 		pe.setAttribute("name", "jnlp_href");
 		pe.setAttribute("value", "isgem.jnlp");
+		
+		Element pe1 = Document.get().createElement("param");
+		pe1.setAttribute("name", "image");
+		pe1.setAttribute("value", "matis2.gif");
+		
+		Element pe2 = Document.get().createElement("param");
+		pe2.setAttribute("name", "centerimage");
+		pe2.setAttribute("value", "true");
+		
+		Element pe3 = Document.get().createElement("param");
+		pe3.setAttribute("name", "boxborder");
+		pe3.setAttribute("value", "false");
+		
+		Element pe4 = Document.get().createElement("param");
+		pe4.setAttribute("name", "draggable");
+		pe4.setAttribute("value", "true");
+		
+		/*<param name="image" value="matis2.gif"/> 
+		<param name="centerimage" value="true"/> 
+		<param name="boxborder" value="false"/>
+		<param name="draggable" value="true"/>*/
+		
 		ae.appendChild( pe );
+		ae.appendChild( pe1 );
+		ae.appendChild( pe2 );
+		ae.appendChild( pe3 );
+		ae.appendChild( pe4 );
 		//HTML applet = new HTML( "<applet width='100%' height='100%' codebase='http://"+server+"/' archive='pontun.jar' code='org.simmi.Order'><param name='jnlp_href' value='order.jnlp' /></applet>" );
 		rp.getElement().appendChild( ae );
 	}
