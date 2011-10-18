2b815a073d535b77bc9d5fb59408049ff67e86bb
root <root@simmi.(none)>
2011-10-18 20:03:20 +0000

    ok

--- vote/src/org/simmi/client/GreetingServiceAsync.java
+++ vote/src/org/simmi/client/GreetingServiceAsync.java
@@ -1,5 +1,7 @@
 package org.simmi.client;
 
+import java.util.Map;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
@@ -8,4 +10,5 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface GreetingServiceAsync {
 	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
 	void saveVote(String key, String name, String vote, AsyncCallback<String> callback);
+	void getVotes(AsyncCallback<Map<String, Integer>> callback);
 }
