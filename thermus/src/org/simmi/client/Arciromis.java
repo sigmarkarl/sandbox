13f099e54762fd2b65429bf9a75fc1e8ac0821ac
root <root@simmi.(none)>
2011-11-02 15:02:55 +0000

    erm

--- thermus/src/org/simmi/client/Arciromis.java
+++ thermus/src/org/simmi/client/Arciromis.java
@@ -1,6 +1,8 @@
 package org.simmi.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
@@ -12,9 +14,15 @@ public class Arciromis implements EntryPoint {
 	public void onModuleLoad() {
 		final RootPanel	rp = RootPanel.get();
 		
+		Window.enableScrolling( false );
 		int w = Window.getClientWidth();
 		int h = Window.getClientHeight();
 		rp.setSize(w+"px", h+"px");
+		
+		Style st = rp.getElement().getStyle();
+		st.setBorderWidth(0.0, Unit.PX);
+		st.setMargin(0.0, Unit.PX);
+		st.setPadding(0.0, Unit.PX);
 		
 		Window.addResizeHandler( new ResizeHandler() {
 			@Override
