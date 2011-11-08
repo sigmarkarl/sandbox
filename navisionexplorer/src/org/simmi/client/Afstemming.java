d0e1857adccc903df0aae7d28ff26689adf92cd3
root <root@simmi.(none)>
2011-11-08 11:21:22 +0000

    erm

---
+++ navisionexplorer/src/org/simmi/client/Afstemming.java
@@ -0,0 +1,38 @@
+package org.simmi.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Afstemming implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		final RootPanel rp = RootPanel.get();
+		Style st = rp.getElement().getStyle();
+		st.setBorderWidth(0.0, Unit.PX);
+		st.setMargin(0.0, Unit.PX);
+		st.setPadding(0.0, Unit.PX);
+		
+		int w = Window.getClientWidth();
+		int h = Window.getClientHeight();
+		rp.setSize(w+"px", h+"px");
+		
+		Window.addResizeHandler( new ResizeHandler() {
+			@Override
+			public void onResize(ResizeEvent event) {
+				int w = event.getWidth();
+				int h = event.getHeight();
+				
+				rp.setSize(w+"px", h+"px");
+			}
+		});
+		
+		Window.enableScrolling( false );
+	}
+
+}
