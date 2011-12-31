8d5e93514c9f4c99a749cb8dc3ef7a852bb49628
root <root@simmi.(none)>
2011-12-31 18:08:48 +0000

    erm

--- thermus/src/org/simmi/client/GreetingServiceAsync.java
+++ thermus/src/org/simmi/client/GreetingServiceAsync.java
@@ -1,10 +1,13 @@
 package org.simmi.client;
 
+import java.util.Map;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * The async counterpart of <code>GreetingService</code>.
  */
 public interface GreetingServiceAsync {
-	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+	void greetServer(String acc, String country, AsyncCallback<String> callback);
+	void getThermus( AsyncCallback<Map<String,String>> callback );
 }
