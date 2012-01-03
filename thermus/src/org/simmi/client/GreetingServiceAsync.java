37a751f8f86b8fedb114f20dd817cbe59c5f8e58
Sigmar Stefansson <sigmar@flx454.matis.local>
2012-01-03 15:24:28 +0000

    ok

--- thermus/src/org/simmi/client/GreetingServiceAsync.java
+++ thermus/src/org/simmi/client/GreetingServiceAsync.java
@@ -2,12 +2,14 @@ package org.simmi.client;
 
 import java.util.Map;
 
+import org.simmi.shared.Chunk;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * The async counterpart of <code>GreetingService</code>.
  */
 public interface GreetingServiceAsync {
-	void greetServer(String acc, String country, AsyncCallback<String> callback);
-	void getThermus( AsyncCallback<Map<String,String>> callback );
+	void greetServer(String acc, String country, boolean valid, AsyncCallback<String> callback);
+	void getThermus( AsyncCallback<Map<String,Chunk>> callback );
 }
