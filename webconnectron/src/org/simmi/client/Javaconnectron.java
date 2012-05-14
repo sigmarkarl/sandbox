537f64704eee587781fe60ab441be7577955349a
root <root@simmi.(none)>
2012-05-14 10:51:11 +0000

    erm

---
+++ webconnectron/src/org/simmi/client/Javaconnectron.java
@@ -0,0 +1,13 @@
+package org.simmi.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Javaconnectron implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
