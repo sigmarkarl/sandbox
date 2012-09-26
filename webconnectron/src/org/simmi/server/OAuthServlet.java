b4d479cc7592916f6da7c06bedbae88c979a3777
Sigmar Stefansson <sigmar@flx454.matis.local>
2012-09-26 09:28:32 +0000

    erm

---
+++ webconnectron/src/org/simmi/server/OAuthServlet.java
@@ -0,0 +1,16 @@
+package org.simmi.server;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OAuthServlet extends HttpServlet {
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		 this.log("hello");
+	 }
+}
