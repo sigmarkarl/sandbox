499c3ba07c18afa20c40b1eade3a4cfb90acccb1
root <root@simmi.(none)>
2012-11-22 10:18:30 +0000

    ok

--- distann/src/org/simmi/shared/GBK2AminoFasta.java
+++ distann/src/org/simmi/shared/GBK2AminoFasta.java
@@ -1,10 +1,5 @@
 package org.simmi.shared;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
