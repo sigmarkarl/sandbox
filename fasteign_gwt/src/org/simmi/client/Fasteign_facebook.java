7fb4d492d8bb732bf5a0c774dff7e17ba663c29b
Sigmar Stefansson <sigmarkarl@gmail.com>
2013-02-25 08:40:00 +0000

    er

--- fasteign_gwt/src/org/simmi/client/Fasteign_facebook.java
+++ fasteign_gwt/src/org/simmi/client/Fasteign_facebook.java
@@ -2,7 +2,6 @@ package org.simmi.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
