de0e34c81c8f0d251a421f2e99b018ed89c0e044
Sigmar Stefansson <sigmarkarl@gmail.com>
2013-08-13 17:47:01 +0000

    m

--- distann/src/org/simmi/GeneSorter.java
+++ distann/src/org/simmi/GeneSorter.java
@@ -12,7 +12,6 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
