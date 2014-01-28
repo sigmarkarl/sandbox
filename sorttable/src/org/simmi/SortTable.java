f801ed520530326d81b340be9f4598e040b4c11f
Sigmar Stefansson <sigmar@sigmac.lan>
2014-01-28 19:43:49 +0000

    e

--- sorttable/src/org/simmi/SortTable.java
+++ sorttable/src/org/simmi/SortTable.java
@@ -33,7 +33,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
