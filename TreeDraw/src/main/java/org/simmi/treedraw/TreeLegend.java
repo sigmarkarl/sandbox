7129fa9a0a0263a9490c3b901431327ca09f0f7e
sigmar <sigmar@wuxinextcode.com>
2020-11-13 13:28:59 +0000

    stuff

--- TreeDraw/src/main/java/org/simmi/treedraw/TreeLegend.java
+++ TreeDraw/src/main/java/org/simmi/treedraw/TreeLegend.java
@@ -93,16 +93,6 @@ public class TreeLegend {
 	
 	public static void main(String[] args) {
 		Map<String,Color> locleg = new HashMap<String,Color>();
-		/*locleg.put("Geysir norður", new Color(0.0f, 0.5f, 1.0f) );
-		locleg.put("Geysir vestur", new Color(0.0f, 1.0f, 0.5f) );
-		locleg.put("Flúðir", new Color(1.0f, 0.0f, 1.0f) );
-		locleg.put("Ölkelduháls", new Color(1.0f, 1.0f, 0.0f) );
-		locleg.put("Hrafntinnusker", new Color(0.0f, 0.0f, 1.0f) );
-		locleg.put("Reykjadalir", new Color(0.0f, 1.0f, 0.0f) );
-		locleg.put("Vondugil", new Color(1.0f, 0.0f, 0.0f) );
-		locleg.put("Hurðarbak", new Color(1.0f, 0.0f, 0.5f) );
-		locleg.put("Kleppjárnsreykir", new Color(1.0f, 0.5f, 0.0f) );
-		locleg.put("Deildartunguhver", new Color(0.5f, 0.0f, 0.5f) );*/
 		
 		makeImage( locleg );
 	}
