c69e2af153d9f3ed904bdc6207c1ffe4c9058b97
Sigmar Stefansson <sigmar@hotmail.com>
2022-03-29 17:16:17 +0000

    start

--- distann/build.gradle.kts
+++ distann/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 javafx {
-    version = "17.0.1"
+    version = "17.0.2"
     modules("javafx.base", "javafx.graphics", "javafx.controls", "javafx.fxml", "javafx.swing", "javafx.web")
 }
 
@@ -14,6 +14,8 @@ dependencies {
     implementation(project(":serifier"))
     implementation(project(":spilling"))
     implementation(project(":TreeDraw"))
+
+    implementation("org.java-websocket:Java-WebSocket:1.5.2")
 
     implementation(group = "org.apache.poi", name = "poi", version = "3.17")
     implementation(group = "org.apache.poi", name = "poi-ooxml", version = "3.17")
@@ -25,20 +27,20 @@ dependencies {
     // https://mvnrepository.com/artifact/org.openjfx/javafx
     implementation(group = "org.openjfx", name = "javafx", version = "17.0.1")
 
-    implementation("org.openjfx:javafx-base:17.0.1:mac")
-    implementation("org.openjfx:javafx-controls:17.0.1:mac")
-    implementation("org.openjfx:javafx-graphics:17.0.1:mac")
-    implementation("org.openjfx:javafx-fxml:17.0.1:mac")
-    implementation("org.openjfx:javafx-web:17.0.1:mac")
-    implementation("org.openjfx:javafx-swing:17.0.1:mac")
+    implementation("org.openjfx:javafx-base:17.0.2:mac")
+    implementation("org.openjfx:javafx-controls:17.0.2:mac")
+    implementation("org.openjfx:javafx-graphics:17.0.2:mac")
+    implementation("org.openjfx:javafx-fxml:17.0.2:mac")
+    implementation("org.openjfx:javafx-web:17.0.2:mac")
+    implementation("org.openjfx:javafx-swing:17.0.2:mac")
 
-    implementation (group = "org.apache.spark", name = "spark-core_2.13", version = "3.2.0") {
+    implementation (group = "org.apache.spark", name = "spark-core_2.13", version = "3.2.1") {
         exclude(group = "avro-mapred")
     }
-    implementation (group = "org.apache.spark", name = "spark-mllib_2.13", version = "3.2.0") {
+    implementation (group = "org.apache.spark", name = "spark-mllib_2.13", version = "3.2.1") {
         exclude(group = "avro-mapred")
     }
-    implementation (group = "org.apache.spark", name = "spark-kubernetes_2.13", version = "3.2.0") {
+    implementation (group = "org.apache.spark", name = "spark-kubernetes_2.13", version = "3.2.1") {
         exclude(group = "avro-mapred")
     }
 
